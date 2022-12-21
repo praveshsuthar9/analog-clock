566e79c2e8d30f55b8c9970b7d3c96061f009138
Pravesh Suthar <111557389+praveshsuthar9@users.noreply.github.com>
2022-12-21 16:21:59 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,23 @@
+const hour = document.getElementById('hr')
+const minute = document.getElementById('min')
+const second = document.getElementById('sec')
+
+
+setInterval(() =>{
+    let date=new Date ();
+    hr=date.getHours()*30;
+    min=date.getMinutes()*6;
+    sec=date.getSeconds()*6;
+
+    hour.style.transform=`rotateZ(${(hr)+(min/12)}deg)`;
+    minute.style.transform=`rotateZ(${min}deg)`;
+    second.style.transform=`rotateZ(${sec}deg)`;
+
+})
+
+
+
+
+
+
+
